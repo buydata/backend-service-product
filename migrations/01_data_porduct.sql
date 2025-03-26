091f37f76cdb2bc4c7fd53ad4d8b9f47409f1463
mark <hostname@internet.ru>
2025-03-26 13:54:39 +0300

    feat: update schema

--- migrations/01_data_porduct.sql
+++ migrations/01_data_porduct.sql
@@ -1,14 +1,14 @@
 -- Your SQL goes here
 DROP TABLE IF EXISTS "data_products";
-CREATE TABLE "data_products" (
-  "id" uuid PRIMARY KEY NOT NULL,
-  "owner_id" uuid  NOT NULL,
-  "status" varchar NOT NULL,
-  "format" varchar NOT NULL,
-  "name" varchar NOT NULL,
-  "category" varchar NOT NULL,
-  "source" varchar NOT NULL,
-  "partitions" int NOT NULL,
-  "created_at" timestamp NOT NULL,
-  "update_at" timestamp NOT NULL
+CREATE TABLE data_products (
+    id UUID PRIMARY KEY,
+    owner_id UUID NOT NULL,
+    status VARCHAR NOT NULL,
+    format VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    category VARCHAR NOT NULL,
+    source VARCHAR NOT NULL,
+    partitions SMALLINT NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
